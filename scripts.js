0fefd28f4aafb2bc21c57510dce744f80414db25
Anna-Kay Graham <annakaygraham123@gmail.com>
2020-11-01 22:13:18 -0500

    JAVASCRIPT FILE

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+const submitBtn = document.getElementsByTagName("button")[0];
+var form = document.getElementsByTagName("form")
+var mail = document.getElementById("email");
+var msg = document.getElementsByClassName("message")
+const validate = (e) => {
+     e.preventDefault();
+  if (mail.value === ""||mail.value==null) {
+    
+    for (var i = 0; i < msg.length; i++) {
+        msg[i].innerHTML = "Please enter a valid email address";
+    }
+    return false;
+    }else{
+        for (var x = 0; x < msg.length; x++) {
+            msg[x].innerHTML = ("Thank you! Your email address "+ mail.value +" has been added to our mailing list!");
+            
+            }
+            return false;  
+    }
+  
+  
+ 
+}
+submitBtn.addEventListener("click", validate);
+
+}
+
+
+
+
+
